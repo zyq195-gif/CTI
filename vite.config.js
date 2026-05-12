a848bf416224cc85cc50ab1180bf9097b345314d
zyq195-gif <17614297972@163.com>
2026-05-12 10:09:48 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue' // 这里是修复的关键点，加上了 plugin-
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  // 必须配置为你 GitHub 的仓库名，前后都要有斜杠
-  base: '/CTI/',
-})

2de1bb9d466fe7561dcff4d5ab9773a6ff73bdab
zyq195-gif <17614297972@163.com>
2026-05-12 10:08:02 +0800

    Delete public directory

--- public/fluid-bg.js
+++
@@ -1,7 +0,0 @@
-<script src="美学流体Bg.min.js"></script>
-
-let colorbg = new Color4Bg.美学流体Bg({
-    dom: "box",
-    colors: ["#b8dbff","#f0fffe","#b8dbff","#F0FFFE","#b8dbff","#F0FFFE"],
-    loop: true
-})
